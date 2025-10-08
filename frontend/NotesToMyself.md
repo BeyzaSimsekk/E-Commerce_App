1a460b6b4c3d823e023be33cfcedb127353d5fc7
Beyza <u.beyza.simsek@gmail.com>
2025-10-08 18:16:33 +0300

    Navbar done

--- frontend/NotesToMyself.md
+++ frontend/NotesToMyself.md
@@ -4,9 +4,13 @@
 
 ### CSS NOTES
 
-- **_hidden_** : Bu element (burada <ul>) varsayılan olarak gizli olur. Yani mobil ekranlarda veya küçük cihazlarda görünmez.
-- **_sm:flex_** : Tailwind’in responsive (duyarlı) bir sınıfıdır. Ekran genişliği 640px veya daha büyükse, display: flex uygula.
-- **_a.active hr { display: block; }_**: Yalnızca aktif olan menü sekmesinin altındaki <hr> çizgisini görünür yapar.
+- **_hidden_** → Bu element (burada <ul>) varsayılan olarak gizli olur. Yani mobil ekranlarda veya küçük cihazlarda görünmez.
+- **_sm:flex_** → Tailwind’in responsive (duyarlı) bir sınıfıdır. Ekran genişliği 640px veya daha büyükse, display: flex uygula.
+- Tailwind’de **_sm_**, bir breakpoint yani _“ekran genişliği eşiği”_ anlamına gelir.
+- **_a.active hr { display: block; }_** → Yalnızca aktif olan menü sekmesinin altındaki <hr> çizgisini görünür yapar.
+- **_leading-4_** → satır yüksekliğini 1rem yapar. Yani <p> içindeki “10” rakamı dikeyde ortalı görünür.
+- **_aspect-square_** → elemanın en ve boyunu eşit yapar (kare şekil). Bu sayede <p> yuvarlak bir rozet (badge) olur.
+- for dynamic classname: use **_className={``}_**, not **_className=""_**.
 
 ### REACT NOTES
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets.js";
 import { NavLink, Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [visible, setVisible] = useState(false);
+
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <img src={assets.logo} className="w-36" alt="Logo" />
-
       {/* Menu Items */}
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1">
@@ -26,7 +27,6 @@ const Navbar = () => {
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
       </ul>
-
       <div className="flex items-center gap-6">
         {/* Search Icon */}
         <img
@@ -60,6 +60,58 @@ const Navbar = () => {
             10
           </p>
         </Link>
+
+        {/* Menu Icon | Responsiveness */}
+        <img
+          onClick={() => setVisible(true)}
+          src={assets.menu_icon}
+          className="w-5 cursor-pointer sm:hidden"
+          alt="Responsive Menu Icon"
+        />
+      </div>
+      {/* Sidebar Menu for small screen | Responsiveness */}
+      <div
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
+          visible ? "w-full" : "w-0"
+        }`}
+      >
+        <div className="flex flex-col text-gray-500">
+          <div
+            onClick={() => setVisible(false)}
+            className="flex items-center gap-4 p-3 cursor-pointer"
+          >
+            <img src={assets.dropdown_icon} className="h-4 rotate-180" alt="" />
+            <p className=" hover:text-fuchsia-950">Back</p>
+          </div>
+          <NavLink
+            onClick={() => setVisible(false)}
+            className="py-2 pl-6 border hover:text-fuchsia-950 hover:bg-[#C586A5] transition-colors duration-200"
+            to="/"
+          >
+            HOME
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            className="py-2 pl-6 border hover:text-fuchsia-950 hover:bg-[#C586A5] transition-colors duration-200"
+            to="/collection"
+          >
+            COLLECTION
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            className="py-2 pl-6 border hover:text-fuchsia-950 hover:bg-[#C586A5] transition-colors duration-200"
+            to="/about"
+          >
+            ABOUT
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            className="py-2 pl-6 border hover:text-fuchsia-950 hover:bg-[#C586A5] transition-colors duration-200"
+            to="/contact"
+          >
+            CONTACT
+          </NavLink>
+        </div>
       </div>
     </div>
   );
