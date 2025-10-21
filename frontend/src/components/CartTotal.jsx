8a3357c3a09d8c311afe41839a60fb839031a1b2
Beyza <u.beyza.simsek@gmail.com>
2025-10-21 15:11:14 +0300

    CartTotal.jsx comp.

--- frontend/src/components/CartTotal.jsx
+++ frontend/src/components/CartTotal.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
 
 const CartTotal = () => {
-  return <div>CartTotal</div>;
+  const { currency, delivery_fee, getCartAmount } = useContext(ShopContext);
+
+  return (
+    <div className="w-full">
+      {/* Title */}
+      <div className="text-2xl">
+        <Title text1={"CART"} text2={"TOTALS"} />
+      </div>
+      <div className="flex flex-col gap-2 mt-2 text-sm">
+        {/* Subtotal */}
+        <div className="flex justify-between">
+          <p>Subtotal</p>
+          <p>
+            {currency} {getCartAmount()}.00
+          </p>
+        </div>
+        <hr className="mt-8 sm:w-4/5 h-[2px] bg-gray-300 border-none" />
+        {/* Delivery Fee */}
+        <div className="flex justify-between">
+          <p>Shipping Fee</p>
+          <p>
+            {currency} {delivery_fee}.00
+          </p>
+        </div>
+        <hr className="mt-8 sm:w-4/5 h-[2px] bg-gray-300 border-none" />
+        {/* Total Amount */}
+        <div className="flex justify-between">
+          <b>Total Amount</b>
+          <b>
+            {currency}{" "}
+            {getCartAmount() === 0 ? 0 : getCartAmount() + delivery_fee}.00
+          </b>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartTotal;
