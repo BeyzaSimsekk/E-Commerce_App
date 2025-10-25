71c2da135639524d79a91b5c8751f93c06be386a
Beyza <u.beyza.simsek@gmail.com>
2025-10-25 19:00:57 +0300

    multer to cloudinary logic

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,4 @@
-
+import {v2 as cloudinary} from 'cloudinary';
 
 // Controller to add a new product
 const addProduct = async (req, res) => {
@@ -11,8 +11,16 @@ const addProduct = async (req, res) => {
         const image3 = req.files.image3 && req.files.image3[0]
         const image4 = req.files.image4 && req.files.image4[0]
 
+        const images = [image1, image2, image3, image4].filter((item) => item !== undefined);
+        let imagesURL = await Promise.all(
+            images.map(async (item) => {
+                let result = await cloudinary.uploader.upload(item.path, {resource_type: "image"});
+                return result.secure_url
+            })
+        )
+
         console.log("----", name, description, price, category, subCategory, sizes, bestSeller);
-        console.log(image1, image2, image3, image4);
+        console.log(imagesURL);
 
         res.status(201).json({}); //message: "Product added successfully" 
 
