1e3ed563de359c738620718984c21923a3637512
Beyza <u.beyza.simsek@gmail.com>
2025-10-25 18:43:56 +0300

    upload image to api with multer from local

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,15 +1,52 @@
 
 
 // Controller to add a new product
-const addProduct = async (req, res) => {};
+const addProduct = async (req, res) => {
+    try {
+        
+        const { name, description, price, category, subCategory, sizes, bestSeller} = req.body;
+
+        const image1 = req.files.image1 && req.files.image1[0]
+        const image2 = req.files.image2 && req.files.image2[0]
+        const image3 = req.files.image3 && req.files.image3[0]
+        const image4 = req.files.image4 && req.files.image4[0]
+
+        console.log("----", name, description, price, category, subCategory, sizes, bestSeller);
+        console.log(image1, image2, image3, image4);
+
+        res.status(201).json({}); //message: "Product added successfully" 
+
+    } catch (error) {
+        console.error("Error adding product:", error);
+        res.status(500).json({success: false, message: "Error adding product", error: error.message });
+    }
+};
 
 // Controller to list products
-const listProducts = async (req, res) => {};
+const listProducts = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
 
 // Controller to remove a product
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
 
 // Controller to get a single product info
-const singleProduct = async (req, res) => {};
+const singleProduct = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
 
 export { addProduct, listProducts, removeProduct, singleProduct };
