252bc71a2ae0ac00c521184d5ea9ebf8487649be
Beyza <u.beyza.simsek@gmail.com>
2025-10-26 00:30:42 +0300

    any api, middleware related to admin done

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -79,6 +79,21 @@ const registerUser = async (req, res) => {
 
 
 // Controller for admin login
-const adminLogin = async (req, res) => {};
+const adminLogin = async (req, res) => {
+    try {
+        
+        const {email, password} = req.body;
+        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+            const token = jwt.sign(email+password, process.env.JWT_SECRET);
+            res.status(200).json({success: true , message: "Admin logged in successfully", data: {token}});
+        } else {
+            res.status(401).json({success: false , message: "Invalid credentials"});
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({success: false , message: "Admin login failed", error: error.message});
+    }
+};
 
 export { loginUser, registerUser, adminLogin };
---
+++ backend/middleware/adminAuth.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+const adminAuth = async (req,res,next) => {
+    try {
+        
+        const token = req.headers.token
+        if(!token) return res.status(401).json({success: false , message: "Not Authorized, No Token"});
+
+        const token_decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if(token_decoded !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) return res.status(401).json({success: false , message: "Not Authorized, Token Failed"});
+
+        next();
+
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({success: false , message: "Not Authorized"});
+        
+    }
+}
+
+export default adminAuth;
