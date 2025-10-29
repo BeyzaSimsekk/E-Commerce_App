fa5ce54ec1914aaee58bd21918b46526d4886cc3
Beyza <u.beyza.simsek@gmail.com>
2025-10-29 17:34:08 +0300

    List.jsx fetchList

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { backendUrl } from "../App";
+import { toast } from "react-toastify";
 
+// { token }
 const List = () => {
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(backendUrl + "/api/product/list");
+      //console.log(response.data);
+      if (response.data.success) {
+        setList(response.data.products);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
   return (
     <div>
       <div></div>
