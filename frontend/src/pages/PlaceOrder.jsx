65771312ae5ec41eb1c1c8f3053353736c237e2f
Beyza <u.beyza.simsek@gmail.com>
2025-10-31 21:41:19 +0300

    updatePlaceOrder.jsx

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -5,11 +5,70 @@ import { assets } from "../assets/assets";
 import { ShopContext } from "../context/ShopContext";
 
 const PlaceOrder = () => {
+  const {
+    navigate,
+    backendUrl,
+    token,
+    cartItems,
+    setCartItems,
+    getCartAmount,
+    ddelivery_fee,
+    products,
+  } = useContext(ShopContext);
   const [method, setMethod] = useState("cod");
-  const { navigate } = useContext(ShopContext);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: "",
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name; // name of the input field
+    const value = event.target.value; // value of the input field
+
+    setFormData((data) => ({ ...data, [name]: value }));
+  };
+
+  /**
+   * items ➜ ürünün ID’si (_id)
+   * item ➜ o ürünün bedeni (size)
+   * cartItems[items][item] ➜ o ürünün o bedenden kaç adet sepette olduğunu gösteriyor
+   */
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      let orderItems = [];
+
+      for (const items in cartItems) {
+        for (const item in cartItems[items]) {
+          if (cartItems[items][item] > 0) {
+            const itemInfo = structuredClone(
+              products.find((product) => product._id === items)
+            );
+            if (itemInfo) {
+              itemInfo.size = item;
+              itemInfo.quantity = cartItems[items][item];
+              orderItems.push(itemInfo);
+            }
+          }
+        }
+      }
+
+      console.log(orderItems);
+    } catch (error) {}
+  };
 
   return (
-    <div className="flex flex-col sm:flex-row justify-between gap-4 p-8 sm:pt-14 min-h-[80vh] border-t border-gray-300  rounded-3xl shadow-lg shadow-[#7c4760]">
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col sm:flex-row justify-between gap-4 p-8 sm:pt-14 min-h-[80vh] border-t border-gray-300  rounded-3xl shadow-lg shadow-[#7c4760]"
+    >
       {/*  ------------- Left Side ------------- */}
       <div className="flex flex-col gap-4 w-full max-w-[480px]">
         <div className="text-xl sm:text-2xl my-2">
@@ -20,11 +79,19 @@ const PlaceOrder = () => {
           {/* Name */}
           <div className="flex gap-3">
             <input
+              required
+              onChange={onChangeHandler}
+              name="firstName"
+              value={formData.firstName}
               className="input-border"
               type="text"
               placeholder="First Name"
             />
             <input
+              required
+              onChange={onChangeHandler}
+              name="lastName"
+              value={formData.lastName}
               className="input-border"
               type="text"
               placeholder="Last Name"
@@ -32,28 +99,76 @@ const PlaceOrder = () => {
           </div>
           {/* Email */}
           <input
+            required
+            onChange={onChangeHandler}
+            name="email"
+            value={formData.email}
             className="input-border"
             type="email"
             placeholder="Email Address"
           />
           {/* Address */}
-          <input className="input-border" type="text" placeholder="Address" />
+          <input
+            required
+            onChange={onChangeHandler}
+            name="street"
+            value={formData.street}
+            className="input-border"
+            type="text"
+            placeholder="Street"
+          />
           {/* City and State */}
           <div className="flex gap-3">
-            <input className="input-border" type="text" placeholder="City" />
-            <input className="input-border" type="text" placeholder="State" />
+            <input
+              required
+              onChange={onChangeHandler}
+              name="city"
+              value={formData.city}
+              className="input-border"
+              type="text"
+              placeholder="City"
+            />
+            <input
+              required
+              onChange={onChangeHandler}
+              name="state"
+              value={formData.state}
+              className="input-border"
+              type="text"
+              placeholder="State"
+            />
           </div>
           {/* Zipcode and Country */}
           <div className="flex gap-3">
             <input
+              required
+              onChange={onChangeHandler}
+              name="zipcode"
+              value={formData.zipcode}
               className="input-border"
               type="number"
               placeholder="Zipcode"
             />
-            <input className="input-border" type="text" placeholder="Country" />
+            <input
+              required
+              onChange={onChangeHandler}
+              name="country"
+              value={formData.country}
+              className="input-border"
+              type="text"
+              placeholder="Country"
+            />
           </div>
           {/* Phone Number */}
-          <input className="input-border" type="number" placeholder="Phone" />
+          <input
+            required
+            onChange={onChangeHandler}
+            name="phone"
+            value={formData.phone}
+            className="input-border"
+            type="number"
+            placeholder="Phone"
+          />
         </div>
       </div>
       {/* ------------- Right Side ------------- */}
@@ -115,13 +230,13 @@ const PlaceOrder = () => {
           </div>
           {/* Buttons */}
           <div className="w-full text-end mt-5">
-            <button onClick={() => navigate("/orders")} className="button1">
+            <button type="submit" className="button1">
               PLACE ORDER
             </button>
           </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
