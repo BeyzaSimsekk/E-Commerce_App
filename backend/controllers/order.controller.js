9ba1da2d9192ad18b8412bf050b52c67c0758c98
Beyza <u.beyza.simsek@gmail.com>
2025-11-02 20:17:12 +0300

    placeOrderStripe controller

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -1,5 +1,14 @@
+import { currency } from "../../admin/src/App.jsx";
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
+import Stripe from 'stripe';
+
+// global variables
+const currency = 'usd'; // currency for payment
+const deliveryCharge = 10; 
+
+// gateway initialization
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // -------------------- PAYMENT FEATURES ---------------------
 
@@ -36,7 +45,53 @@ const placeOrder = async (req,res) => {
 // Placing orders using Stripe Method
 const placeOrderStripe = async (req,res) => {
     try {
-        
+        const {userId, items, amount, address} = req.body; 
+        const {origin} = req.headers;
+
+        const orderData = {
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod: "Stripe",
+            payment: false,
+            date: Date.now(),
+        }
+
+        const newOrder = new orderModel(orderData);
+        await newOrder.save();
+
+        const line_items = items.map((item)=>({
+            price_data: {
+                currency: currency,
+                product_data: {
+                    name: item.name,
+                },
+                unit_amount:item.price * 100, // amount in cents
+            },
+            quantity: item.quantity
+        }))
+
+        line_items.push({
+            price_data: {
+                currency: currency,
+                product_data: {
+                    name: "Delivery Charges",
+                },
+                unit_amount: deliveryCharge * 100, // amount in cents
+            },
+            quantity: 1
+        })
+
+        const session = await stripe.checkout.sessions.create({
+            success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+            line_items,
+            mode: 'payment',
+        })
+
+        res.status(200).json({success:true, session_url:session.url});
+
     } catch (error) {
         console.log(error);
         res.status(500).json({success: false, message: error.message});
